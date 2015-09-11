a8741aa86d722675dcc0567932076071d491e53a
Manuel Ignacio López Quintero <miloqu@gmail.com>
2015-09-12 01:18:51 +0200

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,4 +1,4 @@
-// Compile with: g++ demo.cpp cvrotate2D.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc
+// Compile with: g++ demo.cpp cvrotate2D.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc
 
 #include "cvrotate2D.hpp"
 
