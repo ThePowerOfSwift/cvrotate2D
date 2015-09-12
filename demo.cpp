4aa3916279a9fa99e99a145000237480c719bcaf
Manuel Ignacio López Quintero <miloqu@gmail.com>
2015-09-12 02:42:03 +0200

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -6,12 +6,12 @@ int main()
 {
     const double degrees = 45;
 
-    cv::Mat src = cv::imread("image.png", cv::IMREAD_UNCHANGED);
+    cv::Mat src = cv::imread("frog.png", cv::IMREAD_UNCHANGED);
     cv::Mat dst;
 
     cv::rotate2D(src, dst, degrees);
 
-    cv::imwrite("rotated.png", dst);
+    cv::imwrite("frog_rotated.png", dst);
 
     return 0;
 }
